22ba3a513c88bd182666c97c0e925346d91cdc0e
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-11-16 21:09:53 -0300

    Realizando troca de animações do personagem ao andar e ficar parado.

--- ApocalipseZ/Assets/Scripts/ControlaJogador.cs
+++ ApocalipseZ/Assets/Scripts/ControlaJogador.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class ControlaJogador : MonoBehaviour
 {
     [SerializeField] private float velocidade = 1;
+    private Animator anim;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void FixedUpdate()
     {
@@ -11,6 +16,12 @@ public class ControlaJogador : MonoBehaviour
 
         Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
 
-        transform.Translate(direcao * velocidade* Time.deltaTime);
+        transform.Translate(direcao * velocidade * Time.deltaTime);
+
+        if (direcao != Vector3.zero)
+            anim.SetBool("Correr", true);
+        else
+            anim.SetBool("Correr", false);
+
     }
 }
