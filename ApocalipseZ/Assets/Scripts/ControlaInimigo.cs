54944c9c0f067db4871cc3cefbb95a915de1932b
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-11-19 14:56:37 -0300

    Adicionando rotação ao zumbi para olhar para o player enquanto se movimenta.

--- ApocalipseZ/Assets/Scripts/ControlaInimigo.cs
+++ ApocalipseZ/Assets/Scripts/ControlaInimigo.cs
@@ -4,11 +4,22 @@ public class ControlaInimigo : MonoBehaviour
 {
     [SerializeField] private GameObject jogador;
     [SerializeField] public float velocidade = 5;
+
     private void FixedUpdate()
     {
-        Rigidbody rigidbody = GetComponent<Rigidbody>();
 
-        Vector3 direcao = jogador.transform.position - transform.position;
-        rigidbody.MovePosition(rigidbody.position + direcao.normalized * velocidade * Time.deltaTime);
+        float distancia = Vector3.Distance(transform.position, jogador.transform.position);
+
+        if (distancia > 2)
+        {
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+            Vector3 direcao = jogador.transform.position - transform.position;
+            rigidbody.MovePosition(rigidbody.position + direcao.normalized * velocidade * Time.deltaTime);
+
+            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+
+            rigidbody.MoveRotation(novaRotacao);
+        }
     }
 }
